9208dd0abad8da48893cd533a8ae485fedf04ad7
Jordi <74079212+bljordi78@users.noreply.github.com>
2025-06-06 16:08:57 +0200

    openspace changes

--- utils/openspace.py
+++ utils/openspace.py
@@ -8,6 +8,7 @@ class Openspace:
         self.number_of_tables = len(tables)
 
     
+    # Method to assign the names of the list to the tables created based on length of input file
     def organize(self, names):
          
          random.shuffle(names)
@@ -17,24 +18,24 @@ class Openspace:
                     table.assign_seat(n)
   
 
+     # Method to show the configuration of the tables based on input
+     def display():
+         for num, table in enumerate(self.tables, start=1):
+             print(f"Table {num}:")
+             for seat in table.seats:
+                 occupant = seat.occupant
+                 print(f"\n{occupant}")
+             print("\n")
 
-    # def display():
-    #     for num, table in enumerate(self.tables, start=1):
-    #         print(f"Table {num}:")
-    #         for seat in table.seats:
-    #             attendant = seat.attendant
-    #             print(f"\n{attendantt}")
-    #         print("\n")
-
-    # def store(filename):
-    #         data = []
+     # Method to save the configuration of the table in an excel file
+     def store(filename):
+             data = []
             
-    #         for num, table in enumerate(self.tables, start=1):
-    #             for seat in table.seats:  
-    #                  data.append(
-    #                       f"Table {num} ",
-    #                       seat.attendant 
-    #                  )
-            
-    #         df = pd.DataFrame(data)
-    #         df.to_excel(fileName, index=False)
+             for num, table in enumerate(self.tables, start=1):
+                 for seat in table.seats:  
+                      data.append(
+                           f"Table {num} ",
+                           seat.occupant
+                      )            
+             df = pd.DataFrame(data)
+             df.to_excel(fileName, index=False)
